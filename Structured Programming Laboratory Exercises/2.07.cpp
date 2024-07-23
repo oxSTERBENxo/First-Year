c12e2bb4f3dd248d22323809255d1806f046fb0e
STERBEN <eva.knezevik@gmail.com>
2024-07-23 23:20:14 +0200

    Update 2.07.cpp

--- Structured Programming Laboratory Exercises/2.07.cpp
+++ Structured Programming Laboratory Exercises/2.07.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 int main() {
-    int n,array[100],evenCount=0,oddCount=0;
+    int n,arr[100],evenCount=0,oddCount=0;
     cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> array[i];
+        cin >> arr[i];
     }
     for (int i = 0; i < n; ++i) {
-        if (i % 2 == 0) {  
-            if (array[i] % 2 == 0) {
+        if (i % 2 == 0) {
+            if (arr[i] % 2 == 0) {
                 evenCount++;
             }
-        } else { 
-            if (array[i] % 2 != 0) {
+        } else {
+            if (arr[i] % 2 != 0) {
                 oddCount++;
             }
         }
@@ -24,10 +24,9 @@ int main() {
     double procEvenPos = (double)evenCount / (n / 2) * 100;
     double procOddPos = (double)oddCount / ((n + 1) / 2) * 100;
     if (procEvenPos >= 50 && procOddPos <= 30) {
-        cout << "1" << endl;  // Special array
+        cout << "1" << endl;  
     } else {
-        cout << "0" << endl;  // Not special array
+        cout << "0" << endl;  
     }
     return 0;
 }
-
