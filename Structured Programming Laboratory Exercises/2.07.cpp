cb82cfc1948190f24d8879e8c267c54e6361ce29
STERBEN <eva.knezevik@gmail.com>
2024-07-23 23:20:31 +0200

    Update 2.07.cpp

--- Structured Programming Laboratory Exercises/2.07.cpp
+++ Structured Programming Laboratory Exercises/2.07.cpp
@@ -10,7 +10,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         if (i % 2 == 0) {
             if (arr[i] % 2 == 0) {
                 evenCount++;
