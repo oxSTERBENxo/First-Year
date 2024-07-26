bd147a1fdbc62e033ac7c3a495acda1c8ffeca40
STERBEN <eva.knezevik@gmail.com>
2024-07-26 15:39:51 +0200

    Update 2.42 (ENG).cpp

--- Structured Programming Laboratory Exercises/2.42 (ENG).cpp
+++ Structured Programming Laboratory Exercises/2.42 (ENG).cpp
@@ -10,17 +10,17 @@ using namespace std;
 int main() {
     int n, niza[100];
     cin >> n;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cin >> niza[i];
     }
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         if (niza[i] % 2 == 0) { 
             niza[i] += 3;
         } else if (niza[i] % 5 == 0) { 
             niza[i] -= 2;
         }
     }
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cout << niza[i];
         if (i < n - 1) {
             cout << " "; 
